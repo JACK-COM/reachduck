cee686b07851d022739271618458fedd7298258a
MrJackdaw <MrJackdaw@users.noreply.github.com>
2022-02-05 07:50:00 -0800

    * adds index.network exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./utils/helpers";
 export * from "./session";
 export * from "./storage";
 export * from "./types";
+export * from "./networks/index.networks";
 export * from "./reachlib-api";
